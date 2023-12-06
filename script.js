14acdef89ea8c0a78a9b47584fdc986ed22c5bf0
Roberto Leon-Luna <leonlunadev@gmail.com>
2023-12-06 12:38:37 -0500

    connect javascript to new ui

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  let choices = ["rock", "paper", "scissors"];
+  let choices = ["basketball", "window", "needle"];
   return choices[random()];
 }
 
@@ -11,16 +11,16 @@ function random() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "rock") {
-    if (computerSelection == "paper") {
+  if (playerSelection == "basketball") {
+    if (computerSelection == "needle") {
       computerScore++;
       return "Computer wins round!";
     } else {
       playerScore++;
       return "Player wins round!";
     }
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "scissors") {
+  } else if (playerSelection == "needle") {
+    if (computerSelection == "window") {
       computerScore++;
       return "Computer wins round!";
     } else {
@@ -29,7 +29,7 @@ function playRound(playerSelection, computerSelection) {
     }
   } else {
     // scissors
-    if (computerSelection == "rock") {
+    if (computerSelection == "basketball") {
       computerScore++;
       return "Computer wins round!";
     } else {
@@ -40,19 +40,25 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function handlePlayerSelection(e) {
-  let playerSelection = e.target.textContent.toLowerCase();
-  let computerSelection = getComputerChoice();
+  const playerSelection = e.target.className.toLowerCase();
+  const computerSelection = getComputerChoice();
+  const computerChoice = document.querySelector(".computer .choice");
+  const playerChoice = document.querySelector(".player .choice");
+  const playerSrc = "/images/" + playerSelection + ".png";
+  const computerSrc = "/images/" + computerSelection + ".png";
+  computerChoice.innerHTML = `<img src=${computerSrc}>`;
+  playerChoice.innerHTML = `<img src=${playerSrc}>`;
   updateScore(playerSelection, computerSelection);
 }
 
 function updateScore(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    document.querySelector("#round").textContent = "Tie!";
+    document.querySelector(".roundResult h2").textContent = "Tie!";
   } else {
     let result = playRound(playerSelection, computerSelection);
     document.querySelector("#playerScore").textContent = playerScore;
     document.querySelector("#computerScore").textContent = computerScore;
-    document.querySelector("#round").textContent = result;
+    document.querySelector(".roundResult h2").textContent = result;
   }
   if (computerScore == 5 || playerScore == 5) {
     endGameScreen();
@@ -60,15 +66,16 @@ function updateScore(playerSelection, computerSelection) {
 }
 
 function endGameScreen() {
-  const gameResult = document.querySelector("#round");
+  const gameResult = document.querySelector(".roundResult h2");
   if (computerScore > playerScore) {
     gameResult.textContent = "Computer wins the game!";
   } else {
     gameResult.textContent = "Player wins the game!";
   }
-  const results = document.querySelector(".results");
+  const results = document.querySelector("body");
   const playAgain = document.createElement("button");
   playAgain.textContent = "Play again!?";
+  playAgain.style.fontSize = "45px";
   playAgain.addEventListener("click", () => {
     location.reload();
   });
@@ -79,7 +86,7 @@ function endGameScreen() {
   results.appendChild(playAgain);
 }
 
-const selections = document.querySelectorAll(".selection");
+const selections = document.querySelectorAll(".choices img");
 
 selections.forEach((selection) => {
   selection.addEventListener("click", handlePlayerSelection);
